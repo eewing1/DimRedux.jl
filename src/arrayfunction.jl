11fbf3d67eb43ae2901bbf61d0c666232c9c8cf0
Evan Ewing <eewing@mit.edu>
2023-01-12 21:17:17 -0500

    arrayfunction

---
+++ src/arrayfunction.jl
@@ -0,0 +1,12 @@
+function acceptlist()
+    matrix_list=[]
+    while true:
+        matrix_input=read() #don't know what function to use?
+        if matrix_input="quit"
+            break
+        end
+        push!(matrix_list, matrix_input)
+    end
+end
+
+
